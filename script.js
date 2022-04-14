5b5859194d5b9a1efd633a8fde8656f5429e0acc
Joshua McMiller <jjmcmi113r@gmail.com>
2022-04-14 17:16:24 -0500

    Checkerboard solution

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // Your JS goes here
+// loop through 
+
+checkerboard()
+
+
+
+
+function checkerboard() {
+    for (let e = 0; e < 153; e++) {
+        let tempDiv = document.createElement('div');
+        tempDiv.style.width = "11.1%"
+        tempDiv.style.float = 'left'
+        tempDiv.style.paddingBottom = '11.1%'
+
+        if (e % 2 === 0) {
+            tempDiv.style.backgroundColor = 'red';
+
+        }   else {
+            tempDiv.style.backgroundColor = 'black';
+        }
+        document.body.appendChild(tempDiv)
+
+    
+    }
+}
