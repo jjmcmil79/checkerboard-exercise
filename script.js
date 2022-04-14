9a16737f0da6618854ddf3af7373d76bea4ca842
Joshua McMiller <jjmcmi113r@gmail.com>
2022-04-14 18:01:58 -0500

    Checkerboard random solution

--- script.js
+++ script.js
@@ -25,17 +25,3 @@ function checkerboard() {
     }
 }
 
-function generateRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-  
-  var randomColor=generateRandomColor();//"#F10531"
-
-  console.log(randomColor)
-
-  
